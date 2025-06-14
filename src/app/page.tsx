7555d9915e89d3297464973896b4dbcc6b06951c
registration <gajienko44@gmail.com>
2025-06-14 18:40:36 +0300

    fix: force dynamic rendering to resolve useSearchParams SSR error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
-// components/Sort.tsx
-"use client";
+'use client';
 
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Sort from "@/components/Sort";
+import CarList from "@/components/CarList";
+import Pagination from "@/components/Pagination";
 
-export default function Sort() {
-  const params = useSearchParams();
-  const sort = params.get("sort");
-
-  return <div>Сортировка: {sort}</div>;
+export const dynamic = "force-dynamic";  
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex flex-col gap-6 px-2 py-4 sm:px-4 sm:py-6">
+      <div className="max-w-7xl w-full mx-auto flex flex-col gap-6">
+        <Suspense fallback={null}>
+          <Sort />
+        </Suspense>
+        <Suspense fallback={null}>
+          <CarList />
+        </Suspense>
+        <Suspense fallback={null}>
+          <Pagination />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
